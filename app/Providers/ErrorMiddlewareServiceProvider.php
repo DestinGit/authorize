8723d9c90c0b2e2041691ce05468b864621bf98c
Destin <d.gandobak@gmail.com>
2021-06-09 12:41:08 +0200

    Ajout de Pretty Whoops Error Page

--- app/Providers/ErrorMiddlewareServiceProvider.php
+++ app/Providers/ErrorMiddlewareServiceProvider.php
@@ -4,16 +4,19 @@
 namespace App\Providers;
 
 
+use Zeuxisoo\Whoops\Slim\WhoopsMiddleware;
+
 class ErrorMiddlewareServiceProvider extends ServiceProvider
 {
 
 	public function register()
 	{
-		$this->app->addErrorMiddleware(
-			config('middleware.error_details.displayErrorsDetails'),
-			config('middleware.error_details.logErrors'),
-			config('middleware.error_details.logErrorsDetails')
-		);
+		$this->app->add(new WhoopsMiddleware());
+//		$this->app->addErrorMiddleware(
+//			config('middleware.error_details.displayErrorsDetails'),
+//			config('middleware.error_details.logErrors'),
+//			config('middleware.error_details.logErrorsDetails')
+//		);
 	}
 
 	public function boot()
