aa854b84d1866f38b7dd0379dba2ea237ee76dc9
Destin <d.gandobak@gmail.com>
2021-06-09 10:12:46 +0200

    Ajout du ServiceProvider avec le refactoring du code 2

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use App\Support\Route;
+$app = Route::$app;
+$app->get('/home', function (\Psr\Http\Message\ServerRequestInterface $request, \Psr\Http\Message\ResponseInterface $response) {
+	return view($response, 'auth.home');
+});
 
 Route::get('/', 'WelcomeController@index');
 Route::get('/{name}/{id}', 'WelcomeController@show');
